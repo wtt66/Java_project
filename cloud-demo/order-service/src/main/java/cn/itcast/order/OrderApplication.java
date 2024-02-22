b9c701d16c3977168e29318853f7681c8e811936
wtt <2416717840@qq.com>
2024-02-23 00:13:35 +0800

    1．微服务调用方式 ·基于RestTemplate发起的http请求实现远程调用 .http请求做远程调用是与语言无关的调用，只要知道对方的ip、端口、接口路径、请求参数即可。

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
@@ -13,4 +14,13 @@ public class OrderApplication {
         SpringApplication.run(OrderApplication.class, args);
     }
 
+
+    /**
+     * 创建RestTemplate并注入Spring容器
+     * @return
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,18 +2,30 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+
+        //2.通过RestTemplat发起HTTP请求
+        //2.1.url路径
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        //2.2.发送url路径实现远程调用，并将返回josn对象转化为实体类对象
+        User user = restTemplate.getForObject(url,User.class);
+        //3.封装user到order
+        order.setUser(user);
         // 4.返回
         return order;
     }
